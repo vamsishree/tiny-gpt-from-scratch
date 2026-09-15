7415331075b9b9fdec68b02b85c6a87273eb9c66
Vamsi Shree <86795578+vamsishree@users.noreply.github.com>
2026-09-15 13:53:49 +0200

    Step 74: linear_forward

--- README.md
+++ README.md
@@ -83,7 +83,7 @@ python scaffold.py
 - [x] **71.** run_one_training_step
 - [x] **72.** train_neural_bigram_loop
 - [x] **73.** sample_from_neural_bigram
-- [ ] **74.** linear_forward
+- [x] **74.** linear_forward
 - [ ] **75.** derive_dx_on_paper
 - [ ] **76.** derive_linear_dw_on_paper
 - [ ] **77.** linear_backward_dx
--- model.py
+++ model.py
@@ -604,8 +604,16 @@ def sample_from_neural_bigram(w, start_id, num_tokens, itos):
 
     return decode_ids(ids, itos)
 
-# Step 74 - linear_forward (not yet solved)
-# TODO: implement
+# Step 74 - linear_forward
+def linear_forward(x, w):
+    y = x @ w
+    return {
+        "y": y,
+        "cache": {
+            "x": x,
+            "w": w,
+        },
+    }
 
 # Step 75 - derive_dx_on_paper (not yet solved)
 # TODO: implement
